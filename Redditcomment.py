6274ea5575181297d2e01382affe71357cfe8d6c
ZeDominion <94365578+ZeDominion@users.noreply.github.com>
2024-01-11 18:01:05 +0100

    Update Redditcomment.py

--- Redditcomment.py
+++ Redditcomment.py
@@ -8,18 +8,6 @@ def save_records(filename, records):
     with open(filename, "w") as f:
         json.dump(records, f, indent=4)
 
-def main():
-    USER = "ZeDominion"  # Replace with the Reddit username
-
-    # Set up praw with your credentials
-    reddit = praw.Reddit(
-        client_id='6il3CGWBkKDyPNzIvn1Tlw',        # Replace with your client ID
-        client_secret='f7dJJOqY3N9fryAiEukqsoBknEq39w',# Replace with your client secret
-        user_agent='python:myredditapp:v1.0 (by /u/ZeDominion)',      # Replace with your user agent
-        username=USER,                   # Your Reddit username
-        password='Louati1404!'          # Your Reddit password
-    )
-
     user = reddit.redditor(USER)
 
     # Getting the latest comments
